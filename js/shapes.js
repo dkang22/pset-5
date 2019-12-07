a013a8c72c30b98b1ba1ae666e47bc90afe6ac49
dkang22 <daniellekang24@gmail.com>
2019-12-07 18:38:53 -0500

    deleted filler comments

--- js/shapes.js
+++ js/shapes.js
@@ -5,19 +5,6 @@ window.onload = function() {
     document.getElementById("triangle-sample").onclick = drawTriangleStaff;
     document.getElementById("smile-sample").onclick = drawFaceStaff;
     document.getElementById("pyramid-sample").onclick = drawPyramidStaff;
-
-    // this is how we're connecting our buttons to our JavaScript functions. let's walk through it.
-    //
-    // document.getElementById("some-id")   <-- you need to give each button a unique ID
-    //                                          and access it in this manner
-    //
-    // onclick is an event that is fired when you click something (in our case, a button).
-    // when we give onclick a value, we're telling JavaScript what to do when we click the button.
-    // you should set onclick equal to your function names (i.e., sayHello).
-    //
-    // there are six event listeners being added for the staff solutions. you'll have an
-    // equivalent set of six event listeners for your solutions. the first one is done for you.
-
     document.getElementById("hello").onclick = sayHello;
     document.getElementById("rectangle").onclick = drawRectangle;
     document.getElementById("colored-rectangle").onclick = drawColoredRectangle;
@@ -25,10 +12,6 @@ window.onload = function() {
     document.getElementById("smile").onclick = drawFace;
     document.getElementById("pyramid").onclick = drawPyramid;
 }
-
-/*
- * Exercise 1.
- */
 
 const sayHello = function() {
     let canvas = document.getElementById('student-canvas-1');
@@ -48,10 +31,6 @@ const sayHello = function() {
     context.font = "48px sans-serif";
     context.strokeText(message, 30, 70, 994);
 };
-
-/*
- * Exercise 2.
- */
 
 const drawRectangle = function() {
     let canvas = document.getElementById('student-canvas-2');
@@ -101,10 +80,6 @@ const drawRectangle = function() {
     context.strokeRect(xcoordinate, ycoordinate, width, height);
     context.stroke();
 };
-
-/*
- * Exercise 3.
- */
 
 const drawColoredRectangle = function() {
     let canvas = document.getElementById('student-canvas-3');
@@ -166,10 +141,6 @@ const drawColoredRectangle = function() {
 
 };
 
-/*
- * Exercise 4.
- */
-
 const drawTriangle = function() {
     let canvas = document.getElementById('student-canvas-4');
     let context = canvas.getContext('2d');
@@ -217,9 +188,6 @@ const drawTriangle = function() {
         context.stroke();
     }
 };
-/*
- * Exercise 5.
- */
 
 const drawFace = function() {
     let canvas = document.getElementById('student-canvas-5');
@@ -269,10 +237,6 @@ const drawFace = function() {
         context.closePath();
     }
 };
-
-/*
- * Exercise 6 (extra credit).
- */
 
 const drawPyramid = function() {
     let canvas = document.getElementById('student-canvas-6');
